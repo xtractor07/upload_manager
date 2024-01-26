237b862356ae2511d4390d118ff1f2602c0e04de
xtractor07 <tryy.m3@gmail.com>
2024-01-27 01:40:08 +0530

    Initial-Commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.project.uploadmanager"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.project.uploadmanager"
+        minSdk = 28
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3" // Use the appropriate version
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    /**Compose*/
+    implementation("androidx.compose.ui:ui:1.6.0") // Use the latest version
+    implementation("androidx.compose.material:material:1.6.0") // Use the latest version
+    implementation("androidx.compose.ui:ui-tooling-preview:1.6.0") // For Preview
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.0")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.6.0")
+
+}
---
+++ app/src/main/java/com/project/uploadmanager/MainActivity.kt
@@ -0,0 +1,114 @@
+package com.project.uploadmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AlignInRow()
+        }
+    }
+
+    @Composable
+    private fun Greeting(name: String) {
+        Text(text = name)
+    }
+
+    @Preview
+    @Composable
+    fun PreviewGreeting() {
+        AlignInColumn()
+    }
+
+    /**Understanding Layouts*/
+    @Composable
+    fun MessageCard(name: String) {
+        Row {
+            Column {
+                Text(
+                    text = "Hello ",
+                    color = Color.White
+                )
+                Text(
+                    text = "$name !",
+                    color = Color.White
+                )
+            }
+        }
+    }
+
+    @Composable
+    fun Counter() {
+        var count by remember { mutableStateOf(0) }
+        MaterialTheme{
+            Row {
+                Button(onClick = { count++ }) {
+                    Text("I've been clicked $count times.")
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun BoxExample() {
+        Box {
+            Text(
+                text = "Hello Compose",
+                color = Color.White
+            )
+        }
+    }
+
+    @Composable
+    fun AlignInRow() {
+        Row {
+            Text(
+                text = "First",
+                Modifier.align(Alignment.CenterVertically),
+                color = Color.Cyan,
+            )
+        }
+    }
+
+    @Composable
+    fun AlignInColumn() {
+        Column {
+            Text(
+                text = "First",
+                Modifier.align(Alignment.CenterHorizontally),
+                color = Color.Cyan,
+            )
+            // This Text composable will be centered horizontally in the Column
+        }
+    }
+
+    @Composable
+    fun FillMAxWidthExample() {
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Cenetred Text", Modifier.fillMaxWidth())
+        }
+    }
+}
