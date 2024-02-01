6830525849b50dafd6c19bb7d5762bcd80485cf5
xtractor07 <tryy.m3@gmail.com>
2024-02-02 01:02:10 +0530

    ComposeLearning:
    -> State
    ->
    ->

--- app/src/main/java/com/project/uploadmanager/MainActivity.kt
+++ app/src/main/java/com/project/uploadmanager/MainActivity.kt
@@ -3,11 +3,12 @@ package com.project.uploadmanager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -15,6 +16,7 @@ import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ComposeCompilerApi
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -22,27 +24,67 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AlignInRowExample()
+            Column (
+                Modifier.fillMaxSize()
+            ){
+                val color = remember {
+                    mutableStateOf(Color.Yellow)
+                }
+                ColorBox(
+                    Modifier
+                        .weight(1f)
+                        .fillMaxSize()
+                ){
+                    color.value = it
+                }
+                Box (modifier = Modifier
+                    .background(color.value)
+                    .weight(1f)
+                    .fillMaxSize()
+                )
+            }
         }
-    }
-
-    @Composable
-    private fun Greeting(name: String) {
-        Text(text = name)
     }
 
     @Preview
     @Composable
     fun PreviewGreeting() {
-        AlignInRowExample()
+
+    }
+
+    @Composable
+    fun ColorBox(
+        modifier: Modifier = Modifier,
+        updatedColor: (Color) -> Unit
+    ) {
+
+        Box(
+            modifier = modifier
+                .background(Color.Red)
+                .clickable {
+                    updatedColor (
+                        Color(
+                            Random.nextFloat(),
+                            Random.nextFloat(),
+                            Random.nextFloat(),
+                            1f
+                        )
+                    )
+                }
+        )
+    }
+
+    @Composable
+    private fun Greeting(name: String) {
+        Text(text = name)
     }
 
     /**Understanding Layouts*/
