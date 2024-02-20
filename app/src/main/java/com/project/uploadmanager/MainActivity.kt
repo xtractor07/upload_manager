5db921a4c9f06da7aba5dc52a060625aad7a9760
xtractor07 <tryy.m3@gmail.com>
2024-02-21 00:04:30 +0530

    ComposeLearning:
    -> Canvas
    -> Drawing Shapes (Rect, Circle)
    ->

--- app/src/main/java/com/project/uploadmanager/MainActivity.kt
+++ app/src/main/java/com/project/uploadmanager/MainActivity.kt
@@ -3,14 +3,14 @@ package com.project.uploadmanager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -21,8 +21,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -30,7 +33,37 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AlignInRowExample()
+            MyCanvas()
+        }
+    }
+
+    @Composable
+    fun MyCanvas() {
+        Canvas(
+            modifier = Modifier
+                .padding(20.dp)
+                .size(300.dp)
+        ) {
+            drawRect(
+                color = Color.Black,
+                size = size
+            )
+            drawRect(
+                color = Color.White,
+                topLeft = Offset(100f, 100f),
+                size = Size(100f, 100f),
+                style = Stroke(
+                    width = 3.dp.toPx()
+                )
+            )
+            drawCircle(
+                brush = Brush.radialGradient(
+                    colors = listOf(Color.Red, Color.Yellow, Color.Cyan),
+                    center = center,
+                    radius = 100f
+                ),
+                radius = 100f,
+            )
         }
     }
 
